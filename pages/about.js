9d73fc4324928688a1639696874e9d924c18b804
Michal Penn <mpenn@shutterstock.com>
2023-11-08 13:46:33 -0500

    initial commit

--- pages/about.js
+++ pages/about.js
@@ -3,13 +3,8 @@ import Link from 'next/link'
 
 export default function About() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>About Page</h1>
-        <p className={styles.description}>
-          <Link href="/">&larr; Go Back</Link>
-        </p>
-      </main>
+    <div>
+      <h1>About Page</h1>
     </div>
   )
 }
